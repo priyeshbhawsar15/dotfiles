b35a9a81e4a29f78b5e65d37f9f4e2e7a41e3eb9
priyesh.bhawsar <priyesh.bhawsar@pepsico.com>
2024-10-21 11:21:50 +0530

    buffer change

--- nvim/.config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
   },
   config = function()
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-    vim.keymap.set('n', '<leader>bf', ':Neotree buffers reveal float<CR>', {})
+    vim.keymap.set('n', '<leader><leader>', ':Neotree buffers reveal float<CR>', {})
     require('neo-tree').setup {
       popup_border_style = 'rounded',
       enable_git_status = true,
--- nvim/.config/nvim/lua/kickstart/plugins/telescope.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/telescope.lua
@@ -98,7 +98,7 @@ return {
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      -- vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>sf', builtin.git_files, { desc = '[S]earch [G]it Files' })
 
       -- Slightly advanced example of overriding default behavior and theme
