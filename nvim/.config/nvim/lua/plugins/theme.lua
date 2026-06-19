a8adde11eaedd07f45d94914ab26b10002a349c4
Priyesh Bhawsar <priyeshbhawsar15@gmail.com>
2026-06-19 16:24:05 +0530

    added theme file

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,1 +1,21 @@
-/home/priyesh/.config/omarchy/current/theme/neovim.lua
+local omarchy_theme = vim.fn.expand("~/.config/omarchy/current/theme/neovim.lua")
+
+if vim.fn.filereadable(omarchy_theme) == 1 then
+  return dofile(omarchy_theme)
+end
+
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
